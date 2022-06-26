6c8d2e55f8b9e58ee4b5734515e87b2d325f3f4f
mzgajner <mato@zgajner.com>
2022-06-27 01:29:34 +0200

    Configure real login

--- app.ts
+++ app.ts
@@ -2,37 +2,83 @@ import 'dotenv/config'
 import express, { Express, Request, Response } from 'express'
 import session from 'express-session'
 import passport from 'passport'
-import { PrismaClient } from '@prisma/client'
+import cookieParser from 'cookie-parser'
+import { PrismaClient, User } from '@prisma/client'
 import { PrismaSessionStore } from '@quixo3/prisma-session-store'
+import { Strategy as LocalStrategy } from 'passport-local'
+import { ensureLoggedIn } from 'connect-ensure-login'
 
-import { getLocationListContext, createLocation } from './controllers/location.js'
+declare module 'express-session' {
+  interface SessionData {
+    messages: string[]
+  }
+}
+
+import {
+  getLocationListContext,
+  createLocation,
+} from './controllers/location.js'
 import { getUserListContext } from './controllers/user.js'
-import { getBaseContext } from './controllers/_base.js'
+import { defaultTemplateContext } from './controllers/_base.js'
+import argon2 from 'argon2'
 
+const ensureLogin = ensureLoggedIn()
 const prisma = new PrismaClient()
 const app: Express = express()
 const port = 3000
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
 app.use(
   session({
     cookie: {
-     maxAge: 7 * 24 * 60 * 60 * 1000
+      maxAge: 7 * 24 * 60 * 60 * 1000,
     },
     secret: 'a santa at nasa',
-    resave: true,
-    saveUninitialized: true,
-    store: new PrismaSessionStore(
-      prisma,
-      {
-        checkPeriod: 2 * 60 * 1000,
-        dbRecordIdIsSessionId: true,
-        dbRecordIdFunction: undefined,
-      }
-    )
+    resave: false,
+    saveUninitialized: false,
+    store: new PrismaSessionStore(prisma, {
+      checkPeriod: 2 * 60 * 1000,
+      dbRecordIdIsSessionId: true,
+      dbRecordIdFunction: undefined,
+    }),
   })
 )
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await prisma.user.findUnique({ where: { email: username } })
+      if (user === null) {
+        return done(null, false, { message: 'Napačen email ali geslo.' })
+      } else if (!(await argon2.verify(user.password, password))) {
+        return done(null, false, { message: 'Napačen email ali geslo.' })
+      } else {
+        return done(null, user)
+      }
+    } catch (error) {
+      return done(error)
+    }
+  })
+)
+
+passport.serializeUser(function (user, done) {
+  const prismaUser = user as User
+  done(null, prismaUser.id)
+})
+
+passport.deserializeUser(async function (id: string, done) {
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id },
+    })
+
+    done(null, user)
+  } catch (error) {
+    done(error, null)
+  }
+})
+
 app.use(passport.initialize())
 app.use(passport.session())
 app.use(defaultTemplateContext)
@@ -47,8 +93,22 @@ app.get('/login', async (req: Request, res: Response) => {
   res.render('login')
 })
 
-app.post('/login', async (req: Request, res: Response) => {
-  res.redirect('/')
+app.post(
+  '/login/password',
+  passport.authenticate('local', {
+    successReturnToOrRedirect: '/',
+    failureRedirect: '/login',
+    failureMessage: true,
+  })
+)
+
+app.get('/logout', (req, res, next) => {
+  req.logout((error) => {
+    if (error) {
+      return next(error)
+    }
+    res.redirect('/login')
+  })
 })
 
 // ADMIN
@@ -56,29 +116,41 @@ app.get('/admin', (req: Request, res: Response) => {
   res.redirect('/admin/locations')
 })
 
-app.get('/admin/locations', async (req: Request, res: Response) => {
-  const context = await getLocationListContext(req)
-  res.render('location/list', context)
-})
+app.get(
+  '/admin/locations',
+  ensureLogin,
+  async (req: Request, res: Response) => {
+    const context = await getLocationListContext(req)
+    res.render('location/list', context)
+  }
+)
 
-app.get('/admin/locations/create', async (req: Request, res: Response) => {
-  const context = await getLocationListContext(req)
-  res.render('location/create', context)
-})
+app.get(
+  '/admin/locations/create',
+  ensureLogin,
+  async (req: Request, res: Response) => {
+    const context = await getLocationListContext(req)
+    res.render('location/create', context)
+  }
+)
 
-app.post('/admin/locations/create', async (req: Request, res: Response) => {
-  await createLocation(req)
-  res.redirect('/admin/locations')
-})
+app.post(
+  '/admin/locations/create',
+  ensureLogin,
+  async (req: Request, res: Response) => {
+    await createLocation(req)
+    res.redirect('/admin/locations')
+  }
+)
 
-app.get('/admin/users', async (req: Request, res: Response) => {
+app.get('/admin/users', ensureLogin, async (req: Request, res: Response) => {
   const context = await getUserListContext(req)
   res.render('user/list', context)
 })
 
 
 // API
-app.get('/locations', async (req, res) => {
+app.get('/locations', ensureLogin, async (req, res) => {
   const locations = await prisma.location.findMany()
   res.json(locations)
 })
